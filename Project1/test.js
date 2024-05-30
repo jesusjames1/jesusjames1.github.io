987ebcb5986fc8a6f355effb11512df0c70f98a6
jesusjames1 <orinabel@yahoo.com>
2024-05-30 16:22:53 -0400

    Add files via upload

---
+++ Project1/test.js
@@ -0,0 +1,169 @@
+let buttonclick2 = document.getElementById('jas');
+
+buttonclick2.addEventListener('click', function myFunction(){
+    // Get the value of the input field with id="numb"
+    let x = document.getElementById("number2").value;
+    // If x is Not a Number or less than one or greater than 10
+   console.log(typeof x)
+    let text;
+    if (x < 1 || x > 20) {
+      text = "Please enter a number between 1 and 20";
+    } else {
+      text = "Your current total is: $" + (x*12.99);
+    
+    document.getElementById("inputmessage2").innerHTML = text;
+
+    let cartclick2 = document.getElementById('cartButton2');
+
+cartclick2.addEventListener('click', function myFunction(){
+alert('Your cart has been updated, Click to continue to cart');
+
+document.getElementById("closeForm2").submit();
+})}
+  } );
+
+  let buttonclick3 = document.getElementById('jat');
+
+  buttonclick3.addEventListener('click', function myFunction(){
+      // Get the value of the input field with id="numb"
+      let x = document.getElementById("number3").value;
+      // If x is Not a Number or less than one or greater than 10
+     console.log(typeof x)
+      let text;
+      if (x < 1 || x > 20) {
+        text = "Please enter a number between 1 and 20";
+      } else {
+        text = "Your current total is: $" + (x*12.99);
+      
+      document.getElementById("inputmessage3").innerHTML = text;
+  
+      let cartclick3 = document.getElementById('cartButton3');
+  
+  cartclick3.addEventListener('click', function myFunction(){
+  alert('Your cart has been updated, Click to continue to cart');
+  
+  document.getElementById("closeForm3").submit();
+  })}
+    } );
+
+    let buttonclick4 = document.getElementById('jau');
+
+  buttonclick4.addEventListener('click', function myFunction(){
+      // Get the value of the input field with id="numb"
+      let x = document.getElementById("number4").value;
+      // If x is Not a Number or less than one or greater than 10
+      let text;
+      if (x < 1 || x > 20) {
+        text = "Please enter a number between 1 and 20";
+      } else {
+        text = "Your current total is: $" + (x*12.99);
+      
+      document.getElementById("inputmessage4").innerHTML = text;
+  
+      let cartclick4 = document.getElementById('cartButton4');
+  
+  cartclick4.addEventListener('click', function myFunction(){
+  alert('Your cart has been updated, Click to continue to cart');
+  
+  document.getElementById("closeForm4").submit();
+  })}
+    } );
+
+    let buttonclick5 = document.getElementById('jav');
+
+  buttonclick5.addEventListener('click', function myFunction(){
+      // Get the value of the input field with id="numb"
+      let x = document.getElementById("number5").value;
+      // If x is Not a Number or less than one or greater than 10
+     console.log(typeof x)
+      let text;
+      if (x < 1 || x > 20) {
+        text = "Please enter a number between 1 and 20";
+      } else {
+        text = "Your current total is: $" + (x*12.99);
+      
+      document.getElementById("inputmessage5").innerHTML = text;
+  
+      let cartclick5 = document.getElementById('cartButton5');
+  
+  cartclick5.addEventListener('click', function myFunction(){
+  alert('Your cart has been updated, Click to continue to cart');
+  
+  document.getElementById("closeForm5").submit();
+  })}
+    } );
+
+    let buttonclick6 = document.getElementById('jaw');
+
+  buttonclick6.addEventListener('click', function myFunction(){
+      // Get the value of the input field with id="numb"
+      let x = document.getElementById("number6").value;
+      // If x is Not a Number or less than one or greater than 10
+     console.log(typeof x)
+      let text;
+      if (x < 1 || x > 20) {
+        text = "Please enter a number between 1 and 20";
+      } else {
+        text = "Your current total is: $" + (x*12.99);
+      
+      document.getElementById("inputmessage6").innerHTML = text;
+  
+      let cartclick6 = document.getElementById('cartButton6');
+  
+  cartclick6.addEventListener('click', function myFunction(){
+  alert('Your cart has been updated, Click to continue to cart');
+  
+  document.getElementById("closeForm6").submit();
+  })}
+    } );
+
+
+
+
+    nursery*
+document.getElementById('jar').addEventListener('click', function myFunction(){
+    // Get the value of the input field with id="numb"
+    let amount = document.getElementById("number1").value;
+    // If x is Not a Number or less than one or greater than 10
+    let text1;
+    if (amount < 1 || amount > 20) {
+      text1 = "Please enter a number between 1 and 20";
+      document.getElementById("inputmessage").innerHTML = text1;
+
+    } else {
+      text1 = "Your current total is: $" + (amount*12.99);
+    document.getElementById("inputmessage").innerHTML = text1;}
+
+    let cartclick = document.getElementById('cartButton');
+
+cartclick.addEventListener('click', function myFunction(){
+alert('Your cart has been updated, Click to continue to cart');
+
+document.getElementById("closeForm").submit();
+})}
+  } );
+
+  project1*
+  let buttonclick = document.getElementById('jar');
+
+buttonclick.addEventListener('click', function myFunction(){
+    // Get the value of the input field with id="numb"
+    let x = document.getElementById("number1").value;
+    // If x is Not a Number or less than one or greater than 10
+    let text;
+    if (typeof x !== Number || x < 1 || x > 20) {
+      text = "Please enter a number between 1 and 20";
+    } else {
+      text = "Your current total is: $" + (x*12.99);
+    
+    document.getElementById("inputmessage").innerHTML = text;
+
+    let cartclick = document.getElementById('cartButton');
+
+cartclick.addEventListener('click', function myFunction(){
+alert('Your cart has been updated, Click to continue to cart');
+
+document.getElementById("closeForm").submit();
+
+})}
+} );
